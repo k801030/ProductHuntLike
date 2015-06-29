d5248833145d5c3e790bedb231fb665b5f9b9c13
Vison <kk801030@gmail.com>
2015-06-29 22:33:49 +0800

    import a new tabs component and apply it

--- app/src/main/java/edu/ntu/vison/producthuntlike/Activity/MainActivity.java
+++ app/src/main/java/edu/ntu/vison/producthuntlike/Activity/MainActivity.java
@@ -5,20 +5,24 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
+import com.astuetz.PagerSlidingTabStrip;
+
 import edu.ntu.vison.producthuntlike.R;
 import edu.ntu.vison.producthuntlike.adapter.PagerAdapter;
 
 public class MainActivity extends FragmentActivity {
-    FragmentPagerAdapter pagerAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager vp = (ViewPager) findViewById(R.id.pager);
-        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        vp.setAdapter(pagerAdapter);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+
     }
 
 }
