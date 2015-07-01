1581f142323d3c14041db28363cbac597c9a1f22
Vison <kk801030@gmail.com>
2015-07-01 18:07:35 +0800

    added basic function in addapter view

--- app/src/main/java/edu/ntu/vison/producthuntlike/view/CardStackAdapterView.java
+++ app/src/main/java/edu/ntu/vison/producthuntlike/view/CardStackAdapterView.java
@@ -1,16 +1,22 @@
 package edu.ntu.vison.producthuntlike.view;
 
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ListAdapter;
+
+import java.util.List;
 
 /**
  * Created by Vison on 2015/7/1.
  */
-public class CardStackAdapterView extends AdapterView {
+public class CardStackAdapterView extends AdapterView<ListAdapter> {
     private Adapter mAdapter;
+    private AdapterDataSetObserver mDataSetObserver;
+
 
     public CardStackAdapterView(Context context) {
         super(context);
@@ -25,16 +31,32 @@ public class CardStackAdapterView extends AdapterView {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
     public void setAdapter(Adapter adapter) {
+        if(mAdapter != null && mDataSetObserver != null) {
+            /**
+             *  Calling unregisterDataSetObserver(observer) is not actually mandatory,
+             *  but it's a good practice, and avoids possible memory leaking.
+             *  (you never know what happens with the old adapter, it might get reused,
+             *  so if you skip the unregister part, the custom view will still react to it)
+             */
+            mAdapter.unregisterDataSetObserver(mDataSetObserver);
+        }
         mAdapter = adapter;
+        if(mAdapter != null) {
+            mDataSetObserver = new AdapterDataSetObserver();
+            mAdapter.registerDataSetObserver(mDataSetObserver);
+        }
     }
 
     @Override
     public Adapter getAdapter() {
         return mAdapter;
     }
-
-
 
     @Override
     public View getSelectedView() {
@@ -44,5 +66,18 @@ public class CardStackAdapterView extends AdapterView {
     @Override
     public void setSelection(int i) {
 
+    }
+
+    private class AdapterDataSetObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            requestLayout();
+        }
+        @Override
+        public void onInvalidated() {
+            super.onInvalidated();
+            requestLayout();
+        }
     }
 }
